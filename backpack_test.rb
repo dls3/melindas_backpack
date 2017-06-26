4e2b7bc5a5e04495d3058856dced57a4e790a5e9
Doug <doug.steinberg@gmail.com>
2017-06-26 13:19:04 -0400

    Added test for gym shoes in backpack

--- backpack_test.rb
+++ backpack_test.rb
@@ -44,8 +44,19 @@ class BackpackTest < MiniTest::Test
 
 
   ###### TESTS ABOUT GYM SHOES... wait, where are the tests about gym shoes? #####
-  # is something missing here?
+  def test_gym_shoes_on_gym_days
+    ['monday', 'thursday'].each do |day|
+      backpack = build(:backpack, day_of_week: day)
+      assert(backpack.items.include?('gym shoes'))
+    end
+  end
 
+  def test_no_gym_shoes_on_off_days
+    ['sunday', 'tuesday', 'wednesday', 'friday', 'saturday'].each do |day|
+      backpack = build(:backpack, day_of_week: day)
+      refute(backpack.items.include?('gym shoes'))
+    end
+  end
 
   ##### TESTS ABOUT PRINTING THE LIST #####
   def test_that_backpack_prints_correctly
