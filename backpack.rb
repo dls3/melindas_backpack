1ece9b2db9e6ac2032cca7aeb6d9dd7b48ae5e01
Doug <doug.steinberg@gmail.com>
2017-06-26 14:23:29 -0400

    Added gym_day?, weekday? methods. Passing all tests. No more long methods

--- backpack.rb
+++ backpack.rb
@@ -3,6 +3,8 @@ class Backpack
     @attributes = attributes # a hash containing day_of_week and weather keys
     @items = []
     dress_for_weather
+    pack_gym_shoes
+    pack_lunch
   end
 
   def items
@@ -15,29 +17,58 @@ class Backpack
 
     # Ensure appropriate clothing is added to backpack
     if weather == 'rainy'
-      @items << 'pants'
-      @items << 'shirt'
-      @items << 'umbrella'
+      items_for_rain
     elsif weather == 'cold'
-      @items << 'pants'
-      @items << 'shirt'
-      @items << 'jacket'
+      items_for_cold
     else
-      @items << 'pants'
-      @items << 'shirt'
+      items_for_unspecified_weather
     end
 
-    # Ensure gym shoes are added to backpack if it's a gym day (monday or thursday)
-    if day_of_week == 'monday' || day_of_week == 'thursday'
+  end
+
+  # Ensure gym shoes are added to backpack if it's a gym day (monday or thursday)
+  def pack_gym_shoes
+    day_of_week = @attributes[:day_of_week]
+    if gym_day?
       @items << 'gym shoes'
     end
+  end
 
-    # Bring a packed lunch on all weekdays
-    if day_of_week != 'saturday' && day_of_week != 'sunday'
+  # Bring a packed lunch on all weekdays
+  def pack_lunch
+    day_of_week = @attributes[:day_of_week]
+    if weekday?
       @items << 'packed lunch'
-    elsif false
+    elsif
       @items << 'snacks'
     end
+  end
+
+  def items_for_rain
+    @items << 'pants'
+    @items << 'shirt'
+    @items << 'umbrella'
+  end
+
+  def items_for_cold
+    @items << 'pants'
+    @items << 'shirt'
+    @items << 'jacket'
+  end
+
+  def items_for_unspecified_weather
+    @items << 'pants'
+    @items << 'shirt'
+  end
+
+  def weekday?
+    day_of_week = @attributes[:day_of_week]
+    day_of_week != 'saturday' && day_of_week != 'sunday'
+  end
+
+  def gym_day?
+    day_of_week = @attributes[:day_of_week]
+    day_of_week == 'monday' || day_of_week == 'thursday'
   end
 
   # Prints a summary packing list for Melinda's backpack
